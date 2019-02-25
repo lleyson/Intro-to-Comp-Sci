aa7a524801113e07f507979e31295fe817c13415
lleyson <lleyson@HS-D210-10.MOHS.local>
2019-02-25 13:23:23 -0500

    2/25

--- Lesson 3-Strings/src/NameThatCelebrity.java
+++ Lesson 3-Strings/src/NameThatCelebrity.java
@@ -4,7 +4,12 @@ public class NameThatCelebrity {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+	String s1= "Allan Alda";
+	String s2= "John Wayne";
+	String s3= "Gregory Peck";
+	
+
+
 		
 		
 		
