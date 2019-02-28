bdac632f2333ed506dcf963a56cd2c0d0f9fa187
lleyson <lleyson@HS-D210-10.MOHS.local>
2019-02-28 14:19:49 -0500

    Edit Lesson 3.2

--- Lesson 3-Strings/src/Lesson3point2.java
+++ Lesson 3-Strings/src/Lesson3point2.java
@@ -5,7 +5,16 @@ public class Lesson3point2 {
 		// TODO Auto-generated method stub
 
 		System.out.println("a\\b\\c\"d\"");		
+		System.out.println("\\t is a tab,\\n is a new line,\\\"prints a \"quote\"");
+		System.out.println("\"line one\"\n\\line\ttwo\\");
+		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\ ");
+		System.out.println("\"\\\\\"\\\"\"\n\t\\\"\\\"\"\"\\\n\\\"\"\"\"\\");
+		String Word= "Birthday";
+		System.out.println(Word.toUpperCase());
+		String Word2="it's my party";
 		
+	
+				
 		
 		
 		
