cc16e054f5a7f1d4d93f970b9ab7cbd19cb9d02a
lleyson <lleyson@HS-D210-10.MOHS.local>
2019-03-06 14:18:21 -0500

    Lesson 3.2 8 and 9

--- Lesson 3-Strings/src/Lesson3point2.java
+++ Lesson 3-Strings/src/Lesson3point2.java
@@ -9,11 +9,16 @@ public class Lesson3point2 {
 		System.out.println("\"line one\"\n\\line\ttwo\\");
 		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\ ");
 		System.out.println("\"\\\\\"\\\"\"\n\t\\\"\\\"\"\"\\\n\\\"\"\"\"\\");
-		String Word= "Birthday";
-		System.out.println(Word.toUpperCase());
+		System.out.println("Bithday".toUpperCase());
 		String Word2="it's my party";
-		
-	
+		System.out.println(Word2.substring(0,4)+" "+ Word2.substring(9,10)+" "+"\""+Word2.substring(8,13).toUpperCase()+"\"");
+		String Word3="whose a whats it";
+		System.out.println(Word3.substring(0,6).toUpperCase()+"\n"+"is"+" "+ Word3.substring(8,12).toUpperCase());
+		String Word4="Every Way The Wind Blows";
+		System.out.println(Word4.substring(0,6).toLowerCase()+ Word4.substring(6,10).toUpperCase()+ Word4.substring(10,14).toLowerCase()+"\\"+ Word4.substring(14,19).toUpperCase()+"\\"+ Word4.substring(19,24).toLowerCase() + " "+ 4 );
+		String Word5= "Peter Piper Picked A Peck Pickled Peppers";
+		System.out.println(Word5.substring(19,20)+ Word5.substring(21,35).toLowerCase()+"\t"+ Word5.substring(36,40).toLowerCase()+ Word5.substring(1,12)+ "\t"+ Word5.substring(30,36).toLowerCase()+ "\t");
+	 
 				
 		
 		
