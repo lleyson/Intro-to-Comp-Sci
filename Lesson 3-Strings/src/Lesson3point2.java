b4d718ea617a70ae6872a096231bb2b746f51992
lleyson <lleyson@HS-D110-05.MOHS.local>
2019-03-07 12:08:08 -0500

    Almost finished 3.2

--- Lesson 3-Strings/src/Lesson3point2.java
+++ Lesson 3-Strings/src/Lesson3point2.java
@@ -15,9 +15,15 @@ public class Lesson3point2 {
 		String Word3="whose a whats it";
 		System.out.println(Word3.substring(0,6).toUpperCase()+"\n"+"is"+" "+ Word3.substring(8,12).toUpperCase());
 		String Word4="Every Way The Wind Blows";
-		System.out.println(Word4.substring(0,6).toLowerCase()+ Word4.substring(6,10).toUpperCase()+ Word4.substring(10,14).toLowerCase()+"\\"+ Word4.substring(14,19).toUpperCase()+"\\"+ Word4.substring(19,24).toLowerCase() + " "+ 4 );
-		String Word5= "Peter Piper Picked A Peck Pickled Peppers";
-		System.out.println(Word5.substring(19,20)+ Word5.substring(21,35).toLowerCase()+"\t"+ Word5.substring(36,40).toLowerCase()+ Word5.substring(1,12)+ "\t"+ Word5.substring(30,36).toLowerCase()+ "\t");
+		System.out.println(Word4.substring(0,6).toLowerCase()+ Word4.substring(6,10).toUpperCase()+ Word4.substring(10,14).toLowerCase()+"\\"+ Word4.substring(14,18).toUpperCase()+"\\"+" "+ Word4.substring(19,24).toLowerCase() + " "+ 4 );
+		String Word5= "Peter Piper Picked A Peck of Pickled Peppers";
+		System.out.println(Word5.substring(19,20)+ " "+ Word5.substring(21,37).toLowerCase()+"\t"+ Word5.substring(37,44).toLowerCase()+" "+ Word5.substring(0,12)+ "\t"+ Word5.substring(29,37).toLowerCase()+ "\t");
+		String Word6= "word has 4 letters";
+		System.out.println("\""+Word6.substring(9,10)+"\""+" "+Word6.substring(11,18)+" "+Word6.substring(5,9)+"\""+9+"\""+" "+ Word6.substring(11,18));
+		String Word7="if SUBSTRING is used";
+		System.out.println(Word7.substring(3,13).toLowerCase()+Word7.substring(13,16)+9);
+		String Word8="Jim, Jacky, Johnny, Jill";
+		System.out.println(Word8.substring(0,3).toLowerCase() +"\\"+3+"\n"+Word8.substring(5,10).toUpperCase()+"\\"+5+"\n"+Word8.substring(12,18).toLowerCase()+"\\"+6+"\n"+ Word8.substring(19,24).toUpperCase()+"\\"+4);
 	 
 				
 		
